67ae2c3faf5e95d2d1f33cf5894b93f601187309
George <gms1980@icloud.com>
2021-05-26 13:06:52 -0700

    added idb.js

--- routes/api/comment-Routes.js
+++ routes/api/comment-Routes.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
-const commentRoutes = require('./comment-routes');
-const pizzaRoutes = require('./pizza-routes');
-
-router.use('/comments', commentRoutes);
-router.use('/pizzas', pizzaRoutes);
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
